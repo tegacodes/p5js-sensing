c07303992d051bbc329ac73492a21254f8cfcd96
tegacodes <tega.brain@gmail.com>
2016-11-02 14:40:09 -0400

    first commit:

---
+++ P5-Particle/1-LED-LDR-p5/p5-read-realtime/sketch.js
@@ -0,0 +1,39 @@
+var data;
+var reading =0;
+var currentM;
+
+function setup() {
+  createCanvas(500,500);
+  var url= 'https://api.particle.io/v1/devices/260037000447333439313830/analogvalue?access_token=27280314aec35f59f2530e76ba2e999f3600dfd9';
+  data = loadJSON(url, parseData);
+  currentM=millis();
+}
+
+
+function draw() {
+  fill(255);
+  if(millis()-currentM>2000){
+    readurl();
+    print("read URL");
+    currentM=millis();
+    print("currentM:"+currentM);
+  }
+
+
+}
+
+
+function parseData(data){
+  background(255,0,0);
+  reading = data.result;
+  print("reading:"+reading);
+  text(reading, 10,10);
+  ellipse(200,200,reading,reading);
+
+
+}
+
+function readurl(){
+  var url= 'https://api.particle.io/v1/devices/260037000447333439313830/analogvalue?access_token=27280314aec35f59f2530e76ba2e999f3600dfd9';
+  data = loadJSON(url, parseData);
+}
