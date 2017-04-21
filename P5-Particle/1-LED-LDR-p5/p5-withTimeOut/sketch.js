fe69d23ef67360ebd0e9d5633311be2c2a3a92ff
tegacodes <tega.brain@gmail.com>
2017-04-21 03:18:25 -0400

    yes

--- P5-Particle/1-LED-LDR-p5/p5-withTimeOut/sketch.js
+++ P5-Particle/1-LED-LDR-p5/p5-withTimeOut/sketch.js
@@ -1,29 +1,49 @@
-var x = 0;
-var counter = 0;
-var timer2 = 0;
+var data;
+var reading = 0;
+var counter=0;
+var w=0;
+var easing = 0.05;
+
 
 function setup() {
-  createCanvas(200, 200);
-  // timer = createP('timer 1');
-  // timer2 = createP('timer 2');
+  createCanvas(500, 500);
+  
+ callAPI();
 
-  interval = setInterval(timeIt, 500);
 }
 
-function timeIt() {
-  // timer.html(counter);
-  counter++;
-    background(random(255));
-    console.log('interval!');
-}
+
 
 function draw() {
 
-  stroke(255);
-  line(x, 0, x, height);
+background(255, 0, 0);
+  fill(255);
 
-  x = x + 3;
-  if (x > width) {
-    x = 0;
-  }
+  text(reading, 20, 20);
+
+  //draw circle with easing. 
+  // var targetW = reading;
+  // var dw = targetW - w;
+  // w += dw * easing;
+  ellipse(width / 2, height / 2, reading,reading);
+  //console.log('w:+'+w);
+
+}
+
+
+function parseData(data) {
+
+  reading = data.result;
+  print("reading:" + reading);
+  //call API every 1000 milliseconds
+  setTimeout(callAPI(), 1000);
+
+}
+
+function callAPI() {
+  var url = 'https://api.particle.io/v1/devices/3a0040000851353531343431/analogvalue?access_token=27280314aec35f59f2530e76ba2e999f3600dfd9';
+  data = loadJSON(url, parseData);
+  counter++;
+//  console.log(counter);
+
 }
